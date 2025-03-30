b81ec1e77d23ab89ac56dcb3ca6b85ba1bac5475
zamirovskiyv <vitaliyzam@gmail.com>
2025-03-30 21:23:53 +0200

    fix: add finished homework s1-w1

--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -1,23 +1,23 @@
 import {CurrentUser} from './CurrentUser';
-import {UsersObjectType} from './HW2';
+import {UsersObjectType, UserType} from './HW2';
 
 type UserList2PropsType = {
-	users: any // пропиши типизацию
-	filterUsers: any // пропиши типизацию
+    users: UsersObjectType
+    filterUsers: () => void
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
-	return (
-		<div id={'hw02-users'}>
-			<h2>User List:</h2>
+    return (
+        <div id={'hw02-users'}>
+            <h2>User List:</h2>
 
-			{/*<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>*/}
+            <button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
 
-			{/*<ul>*/}
-			{/*  {props.users.myFriends.map((user) => (*/}
-			{/*    <ВЫЗЫВАЕМ КОМПОНЕНТУ CurrentUser />*/}
-			{/*  ))}*/}
-			{/*</ul>*/}
-		</div>
-	);
+            <ul>
+                {props.users.myFriends.map((user: UserType) => (
+                    <CurrentUser key={user.id} user={user}/>
+                ))}
+            </ul>
+        </div>
+    );
 };
