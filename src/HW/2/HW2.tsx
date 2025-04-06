e0ec7c8bda0592500ac76863108e38e794a7e1ca
zamirovskiyv <vitaliyzam@gmail.com>
2025-04-06 07:28:22 +0200

    fix: edit s1-w2

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -47,7 +47,7 @@ export const HW2 = () => {
     let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
     const filterUsers = () => {
-        const filteredUsers = users.myFriends.filter(fr => fr.address.city === 'Los Angeles');
+        const filteredUsers = users.myFriends.filter((fr:UserType) => fr.address.city === 'Los Angeles');
         setCurrentUsers({myFriends: filteredUsers});
     };
 
--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -11,7 +11,7 @@ export const UserList2 = (props: UserList2PropsType) => {
         <div id={'hw02-users'}>
             <h2>User List:</h2>
 
-            <button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
+            <button id={'hw02-filter-button'} onClick={() => props.filterUsers()}>SHOW ME FRIENDS FROM LA</button>
 
             <ul>
                 {props.users.myFriends.map((user: UserType) => (
